6a83613b82a5f9a686d07d1c6a66f3e6b92a81b8
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2018-07-23 12:20:28 +0300

    Metronome object and audio files added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+let metronome =  {
+  tempo: 60,
+  sound: audio,
+  timer: null,
+  getTempo() {
+    return this.tempo
+  },
+  setTempo(newTempo) {
+    this.tempo = newTempo
+    console.log(this.tempo) 
+  },
+  tick() {
+    audio.play()
+    let t1 = performance.now()
+    this.timer = setTimeout(() => {
+      this.tick()
+      console.log(performance.now() - t1)
+    }, 60000 / this.getTempo())
+  },
+  stop() {
+    clearInterval(this.timer)
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav')
   var instances = M.Sidenav.init(elems)
