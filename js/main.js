88f4f0406f9eb9b897788fe22605ec08f82925b2
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2018-07-24 06:25:48 +0300

    Trainer object and functionality added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let metronome =  {
   tempo: 60,
   sound: audio,
+  clickCount: 0,
   timer: null,
   getTempo() {
     return this.tempo
@@ -9,17 +10,36 @@ let metronome =  {
     this.tempo = newTempo
     console.log(this.tempo) 
   },
+  getClickCount() {
+    return this.clickCount
+  },
+  setClickCount(newClickCount) {
+    this.clickCount = newClickCount
+  },
   tick() {
     audio.play()
     let t1 = performance.now()
+    this.setClickCount(this.getClickCount() + 1)
+
+    if (this.getClickCount() % trainer.period === 0) {
+      this.setTempo(this.getTempo() + trainer.increment)
+    }
+
     this.timer = setTimeout(() => {
-      this.tick()
       console.log(performance.now() - t1)
+      this.tick()
     }, 60000 / this.getTempo())
   },
   stop() {
     clearInterval(this.timer)
   }
+}
+
+let trainer = {
+  // active: true,
+  period: 4,
+  break: 0,
+  increment: 10
 }
 
 document.addEventListener('DOMContentLoaded', function() {
