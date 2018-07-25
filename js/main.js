49daa5e47ad368018e4eda7bf015a85e096ec778
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2018-07-25 22:53:06 +0300

    Break and limit added

--- js/main.js
+++ js/main.js
@@ -17,11 +17,28 @@ let metronome =  {
     this.clickCount = newClickCount
   },
   tick() {
+    if (
+        (trainer.break > 0) && 
+        (trainer.breakCounter < trainer.break) && 
+        (this.getClickCount() % trainer.period === 0) && 
+        (metronome.getClickCount() > 0)
+    ) {
+      trainer.breakCounter++
+      trainer.isOnBreak = true
+      metronome.setClickCount(metronome.getClickCount() - 1)
+      audio.volume = 0
+      console.log('BREAK')
+    } else {
+      audio.volume = 1
+      trainer.breakCounter = 0
+      trainer.isOnBreak = false
+    }
+
     audio.play()
     let t1 = performance.now()
     this.setClickCount(this.getClickCount() + 1)
 
-    if (this.getClickCount() % trainer.period === 0) {
+    if ((this.getClickCount() % trainer.period === 0) && (this.getTempo() < trainer.limit) && (trainer.breakCounter == 0)) {
       this.setTempo(this.getTempo() + trainer.increment)
     }
 
@@ -36,10 +53,14 @@ let metronome =  {
 }
 
 let trainer = {
-  // active: true,
+  active: true,
+  preCount: false,
   period: 4,
   break: 0,
-  increment: 10
+  breakCounter: 0,
+  isOnBreak: false,
+  increment: 10,
+  limit: 120
 }
 
 document.addEventListener('DOMContentLoaded', function() {
