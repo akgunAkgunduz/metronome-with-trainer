4838ffb0f9dc53404c0742cca5ffdda7bfe07416
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2018-07-27 15:11:50 +0300

    View object and counters added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+const bpm = document.getElementById('bpm')
+const bpmRange = document.getElementById('bpmRange')
+const start = document.getElementById('start')
+const stop = document.getElementById('stop')
+const clickCounter = document.getElementById('clickCounter')
+const breakCounter = document.getElementById('breakCounter')
+
 let metronome =  {
-  active: false,
+  isActive: false,
   sound: audio,
   tempo: 60,
   clickCount: 0,
@@ -18,12 +25,7 @@ let metronome =  {
     this.clickCount = newClickCount
   },
   tick() {
-    if (
-        (trainer.break > 0) && 
-        (trainer.breakCounter < trainer.break) && 
-        (this.getClickCount() % trainer.period === 0) && 
-        (metronome.getClickCount() > 0)
-    ) {
+    if ((trainer.break > 0) && (trainer.breakCounter < trainer.break) && (this.getClickCount() % trainer.period === 0) && (metronome.getClickCount() > 0) && (this.isActive == true)) {
       trainer.breakCounter++
       trainer.isOnBreak = true
       metronome.setClickCount(metronome.getClickCount() - 1)
@@ -36,9 +38,10 @@ let metronome =  {
     }
 
     audio.play()
-    this.active = true
+    this.isActive = true
     let t1 = performance.now()
     this.setClickCount(this.getClickCount() + 1)
+    view.update()
 
     if (trainer.break == 0) {
       if ((this.getClickCount() % trainer.period === 0) && (this.getTempo() < trainer.limit) && (trainer.breakCounter == 0)) {
@@ -57,35 +60,51 @@ let metronome =  {
   },
   stop() {
     clearInterval(this.timer)
+    this.isActive = false
+    this.setClickCount(0)
     trainer.breakCounter = 0
-    this.active = false
+    view.update()
   }
 }
 
 let trainer = {
-  active: true,
+  isActive: true,
   preCount: false,
-  period: 4,
-  break: 2,
+  period: 8,
+  break: 4,
   breakCounter: 0,
   isOnBreak: false,
   increment: 1,
   limit: 280
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  const stop = document.getElementById('stop')
+let view = {
+  clickCount: 0,
+  update() {
+    bpm.value = metronome.getTempo()
+    bpmRange.value = metronome.getTempo()
 
+    view.clickCount = metronome.clickCount % trainer.period
+    if (view.clickCount == 0 && metronome.isActive) {
+      view.clickCount = trainer.period
+    } 
+    clickCounter.textContent = view.clickCount
+    breakCounter.textContent = trainer.breakCounter
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
   M.AutoInit()
 
-  bpm.addEventListener('input', (e) => {
-    metronome.setTempo(e.target.value)
-    bpmRange.value = e.target.value
+  bpm.addEventListener('input', () => {
+    metronome.setTempo(parseInt(bpm.value))
+    bpmRange.value = metronome.getTempo()
   })
 
-  bpmRange.addEventListener('input', (e) => {
-    metronome.setTempo(e.target.value)
-    bpm.value = e.target.value
+  bpmRange.addEventListener('input', () => {
+    metronome.setTempo(parseInt(bpmRange.value))
+    console.log(metronome.getTempo())
+    bpm.value = metronome.getTempo()
   })
 
   start.addEventListener('click', () => {
